d98b27e541019b19f5510d43f91ca9f7a60732da
BenjaminPmt <benjamin.pimont@laposte.net>
2022-08-18 21:11:57 +0200

    site vitrine pizzeria avec responsive

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/library/bootstrap.css">
+    <link rel="stylesheet" href="css/library/fontawesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="icon" href="assets/img/pizza.png">
+    <title>Pizza</title>
+</head>
+<body>
+<header>
+    <ul class="nav-list">
+        <li class="nav-item"><a href="#pizza">Nos Pizzas</a></li>
+        <li class="nav-item"><a href="#horaire">Horaires</a></li>
+        <li class="nav-item"><a href="#contact">Contact</a></li>
+    </ul>
+</header>
+<div>
+    <!-- PIZZA -->
+    <div class="container-row" id="pizza">
+        <div class="container-title">
+            <span class="tittle-one">Nos</span>
+            <span class="tittle-two">Pizzas</span>
+        </div>
+        <div class="presentation-tittle">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, id ducimus iusto suscipit quis ea ipsum perferendis! Porro modi cum aut inventore distinctio, id tempora consequatur, in corporis ipsam quis.
+            Totam hic minus doloremque aliquid, qui nostrum consectetur illum consequatur voluptatem sapiente aut enim voluptates velit incidunt, ex repudiandae quos vero cum cumque officia quis ab. Nihil amet odit aspernatur.
+            Quae minima aut non aliquam, ad at rerum dolore provident voluptatem iure cupiditate ex fugiat mollitia!
+            </p>
+        </div>
+    </div>
+    <div class="grille">
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_1.jpg" alt="">
+            <div class="ingredient">
+                <h2>La Curry</h2>
+                <ul>
+                    <li><img src="assets/img/tomate.png" alt="">Sauce tomate</li>
+                    <li><img src="assets/img/poivron.png" alt="">Poivron</li>
+                    <li><img src="assets/img/curry.png" alt="">Curry</li>
+                    <li><img src="assets/img/oignon.png" alt="">Oignon rouge</li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_2.jpg" alt="">
+            <div class="ingredient">
+                <h2>La Mexicaine</h2>
+                <ul>
+                    <li><img src="assets/img/tomate.png" alt="">Sauce tomate</li>
+                    <li><img src="assets/img/sauce-barbecue.png" alt="">Sauce barbecue</li>
+                    <li><img src="assets/img/poivron.png" alt="">Poivron</li>
+                    <li><img src="assets/img/oignon.png" alt="">Oignon rouge</li>
+                    <li><img src="assets/img/mais.png" alt="">Maïs</li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_3.jpg" alt="">
+            <div class="ingredient">
+                <h2>L'italienne</h2>
+                <ul>
+                    <li><img src="assets/img/tomate.png" alt="">Tomate cerise</li>
+                    <li><img src="assets/img/epinard.png" alt="">Epinard</li>
+                    <li><img src="assets/img/fromage.png" alt="">Fromage</li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_4.jpg" alt="">
+            <div class="ingredient">
+                <h2>La Végétarienne</h2>
+                <ul>
+                    <li><img src="assets/img/tomate.png" alt="">Sauce tomate</li>
+                    <li><img src="assets/img/poivron.png" alt="">Poivron</li>
+                    <li><img src="assets/img/olive.png" alt="">Olive</li>
+                    <li><img src="assets/img/oignon.png" alt="">Oignon rouge</li>
+                    <li><img src="assets/img/mais.png" alt="">Maïs</li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_5.jpg" alt="">
+            <div class="ingredient">
+                <h2>La Chorizo</h2>
+                <ul>
+                    <li><img src="assets/img/tomate.png" alt="">Sauce tomate</li>
+                    <li><img src="assets/img/chorizo.png" alt="">Chorizo</li>
+                    <li><img src="assets/img/fromage.png" alt="">Fromage</li>
+                    <li><img src="assets/img/steak.png" alt="">Steak haché</li>
+                </ul>
+            </div>
+        </div>
+        <div class="grid-item container-ingredient">
+            <img class="grid-img" src="assets/img/pizza_6.jpg" alt="">
+            <div class="ingredient">
+                <h2>La Cannibale</h2>
+                <ul>
+                    <li><img src="assets/img/jambon-de-porc.png" alt="">Jambom</li>
+                    <li><img src="assets/img/champignon.png" alt="">Champignon</li>
+                    <li><img src="assets/img/chorizo.png" alt="">Merguez</li>
+                    <li><img src="assets/img/olive.png" alt="">Olive</li>
+                    <li><img src="assets/img/poivron.png" alt="">Poivron</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- HORAIRES -->
+    <div class="container-row" id="horaire">
+        <div class="container-title">
+            <span class="tittle-one">Nos</span>
+            <span class="tittle-two">Horaires</span>
+        </div>
+        <div class="presentation-tittle">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Midi</th>
+                        <th>Soir</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>Lundi</th>
+                        <td>Fermé</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Mardi</th>
+                        <td>11:00 - 14:00</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Mercredi</th>
+                        <td>11:00 - 14:00</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Jeudi</th>
+                        <td>11:00 - 14:00</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Vendredi</th>
+                        <td>11:00 - 14:00</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Samedi</th>
+                        <td>11:00 - 14:00</td>
+                        <td>18:30 - 23:00</td>
+                    </tr>
+                    <tr>
+                        <th>Dimanche</th>
+                        <td>Fermé</td>
+                        <td>Fermé</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- CONTACT -->
+    <div class="container-row" id="contact">
+        <div class="container-title">
+            <span class="tittle-one">Nous</span>
+            <span class="tittle-two">Contacter</span>
+        </div>
+        <div class="container-contact">
+            <span><img src="assets/img/pizza2.png" alt="">Telephone : xx-xx-xx-xx-xx</span>
+            <span><img src="assets/img/pizza2.png" alt="">Mail : delpizzeria@pizza.fr </span>
+        </div>
+    </div>
+</div>
+<!-- <footer>
+<p>Photo by <a href="https://unsplash.com/@anastasiiachepinska?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anastasiia Chepinska</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by <a href="https://unsplash.com/@rahulupadhyay27?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Rahul Upadhyay</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by <a href="https://unsplash.com/@thisisnando?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Fernando Andrade</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by <a href="https://unsplash.com/es/@tarikul_islam?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Shardar Tarikul Islam</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by <a href="https://unsplash.com/@vinitha_v?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Vinitha V</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by <a href="https://unsplash.com/@catiaclimovich?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Catia Climovich</a> on <a href="https://unsplash.com/s/photos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p>Photo by<a href="https://unsplash.com/@nik_owens?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nik Owens</a> en <a href="https://unsplash.com/es/s/fotos/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+</p>
+<p><a href="https://www.flaticon.com/fr/icones-gratuites/pizza" title="pizza icônes">Pizza icônes créées par Freepik - Flaticon</a></p>
+</footer> -->
+<script src="js/library/jquery.js"></script>
+<script src="js/library/bootstrap.js"></script>
+<script src="js/library/all.js"></script> <!--  script pour fontawesome-->
+<script src="js/js.js"></script>
+
+</body>
+</html>
---
+++ js/js.js
@@ -0,0 +1,6 @@
+// const ingredient = document.querySelector(".container-ingredient");
+// const enlever = document.querySelector('.ingredient');
+
+// ingredient.addEventListener('mouseenter', function(){
+//     $(".ingredient").css({'display': "block"})
+// })
