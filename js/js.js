28e88b6d55c003aeedd1a218e711605431a44435
BenjaminPmt <benjamin.pimont@laposte.net>
2022-08-19 17:31:23 +0200

    ajout des alt et suppression des librairies

--- js/js.js
+++
@@ -1,6 +0,0 @@
-// const ingredient = document.querySelector(".container-ingredient");
-// const enlever = document.querySelector('.ingredient');
-
-// ingredient.addEventListener('mouseenter', function(){
-//     $(".ingredient").css({'display': "block"})
-// })
